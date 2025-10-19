a09ea5a9336d5a9c3aeccca0476f95d99c3a0f6a
aubur <wzr0008@auburn.edu>
2025-10-18 18:36:59 -0700

    Adding sum

--- src/addition.rs
+++ src/addition.rs
@@ -1,5 +1,7 @@
 #[allow(dead_code)]
 pub mod primary_set {
+    use std::cmp::min;
+    use std::fmt::Error;
     use std::ops::Add;
 
     pub fn addition(upperbound: u32) -> u128 {
@@ -23,5 +25,49 @@ pub mod primary_set {
         T: Add<Output = T>,
     {
         a + b
+    }
+
+    pub struct MinStack<T>
+    where
+        T: Ord + PartialOrd,
+    {
+        stack: Vec<T>,
+        stack_min: Vec<T>,
+    }
+    impl<T> MinStack<T>
+    where
+        T: Ord + PartialOrd + Copy,
+    {
+        pub fn new() -> Self {
+            let stack = Vec::new();
+            let stack_min = Vec::new();
+            return Self { stack, stack_min };
+        }
+        pub fn push(&mut self, val: T) {
+            if self.stack_min.len() > 0 {
+                let mut min_val = *self.stack_min.iter().last().unwrap();
+                min_val = min(min_val, val);
+                self.stack_min.push(min_val);
+            } else {
+                self.stack_min.push(val);
+            }
+            self.stack.push(val);
+        }
+        pub fn pop(&mut self) {
+            self.stack_min.pop();
+            self.stack.pop();
+        }
+        pub fn top(&self) -> Result<&T, String> {
+            if self.stack_min.len() == 0 {
+                return Err("The stack is empty".to_string());
+            }
+            return Ok(self.stack.iter().last().unwrap());
+        }
+        pub fn min_value(&self) -> Result<&T, String> {
+            if self.stack_min.len() == 0 {
+                return Err("The stack is empty".to_string());
+            }
+            return Ok(self.stack_min.iter().last().unwrap());
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,17 @@ fn main() {
     let a = 3.5;
     let b = 1.2;
     println!("a+b={}", add_up(a, b));
+    println!("Test the min stack");
+    let mut stack = MinStack::new();
+    stack.push(-1);
+    stack.push(3);
+    println!("The min value of stack is {:?}", stack.min_value());
+    println!("The last value of stack is {:?}", stack.top());
+    stack.pop();
+    stack.pop();
+    stack.push(-4);
+    stack.push(-5);
+    stack.push(100);
+    println!("The min value of stack is {:?}", stack.min_value());
+    println!("The last value of stack is {:?}", stack.top());
 }
